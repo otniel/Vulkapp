656805aec3205343aab6021f0beffd4dffcdf1c8
Otniel Aguilar <otnieel.aguilar@gmail.com>
2016-07-23 11:26:49 -0500

    Empty Android Project

---
+++ app/src/main/java/mx/otniel/vulkapp/AskServiceActivity.java
@@ -0,0 +1,13 @@
+package mx.otniel.vulkapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AskServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ask_service);
+    }
+}
